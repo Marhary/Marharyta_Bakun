f5a6f3acc2f5f024fb413e2add712acf0e3772ab
Marhary <marharyta_bakun@mail.ru>
2016-10-06 22:36:30 +0300

    new changes
    SharedPreferences

--- SaveActivity/app/src/main/java/com/example/margo/saveactivity/MainActivity.java
+++ SaveActivity/app/src/main/java/com/example/margo/saveactivity/MainActivity.java
@@ -2,19 +2,36 @@ package com.example.margo.saveactivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, ActivityTwo.class);
+        final SharedPreferences prefer = getPreferences(MODE_PRIVATE);
+        final Intent intent;
+        if (checkFirstLaunch(prefer)) {
+            saveInstance(prefer);
+            intent = new Intent(this, ActivityTwo.class);
+        }
+        else
+            intent=new Intent(this,ActivitySeven.class);
         startActivity(intent);
+    }
+
+   private void saveInstance(SharedPreferences pref){
+       SharedPreferences.Editor prefEdit=pref.edit();
+       prefEdit.putBoolean("first_launch",false);
+       prefEdit.apply();
+   }
+
+    private boolean checkFirstLaunch(SharedPreferences pref){
+        return pref.getBoolean("first_launch",true);
     }
 }
