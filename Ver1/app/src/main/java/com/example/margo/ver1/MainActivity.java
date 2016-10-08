f6f96a133a14d366d1b456bb34ddb4be47f26ab0
Marhary <marharyta_bakun@mail.ru>
2016-10-08 17:59:38 +0300

    Chenges in project

--- Ver1/app/src/main/java/com/example/margo/ver1/MainActivity.java
+++ Ver1/app/src/main/java/com/example/margo/ver1/MainActivity.java
@@ -1,82 +1,69 @@
 package com.example.margo.ver1;
 
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.TextureView;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Switch;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioGroup radioGroup;
-    TextView textView;
-    CheckBox checkBoxBold;
-    CheckBox checkBoxItelic;
-    Switch showText;
+    private EditText mKmPerHourEditText;
+    private EditText mMeterPerSecEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        textView = (TextView) findViewById(R.id.textView);
-        checkBoxBold = (CheckBox) findViewById(R.id.bold);
-        checkBoxItelic = (CheckBox) findViewById(R.id.itelic);
-        showText = (Switch) findViewById(R.id.switchtext);
+        mKmPerHourEditText = (EditText) findViewById(R.id.editTextKmPerHour);
+        mMeterPerSecEditText = (EditText) findViewById(R.id.editTextMetersPerSec);
 
-
-        checkBoxBold.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (checkBoxBold.isChecked()) {
-                    textView.setTypeface(null, Typeface.BOLD);
-                } else textView.setTypeface(null, Typeface.NORMAL);
-            }
-        });
-
-        checkBoxItelic.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (checkBoxItelic.isChecked()) {
-                    textView.setTypeface(null, Typeface.ITALIC);
-                } else textView.setTypeface(null, Typeface.NORMAL);
-            }
-        });
-
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId) {
-                    case (R.id.radioButton):
-                        textView.setTextColor(Color.MAGENTA);
-                        break;
-                    case (R.id.radioButton1):
-                        textView.setTextColor(Color.CYAN);
-                        break;
-                    case (R.id.fish):
-                        textView.setTextColor(Color.DKGRAY);
-                        break;
-
+        mKmPerHourEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_UP) {
+                    try {
+                        double kmPerHour = Double.parseDouble(mKmPerHourEditText
+                                .getText().toString());
+                        double meterPerSec = kmPerHour * 0.2777777777777778;
+                        mMeterPerSecEditText.setText(Double.valueOf(meterPerSec)
+                                .toString());
+                    } catch (NumberFormatException e) {
+                        mMeterPerSecEditText.setText(R.string.errorMsg);
+                        Log.d("Test", "e:" + e);
+                    }
+                    return true;
                 }
+                return false;
+            }
+        });
+
+
+        mMeterPerSecEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_UP) {
+                    try {
+                        double meterPerSec = Double.parseDouble(mMeterPerSecEditText
+                                .getText().toString());
+                        double kmPerHour = meterPerSec * 3.6;
+                        mKmPerHourEditText.setText(Double.valueOf(kmPerHour)
+                                .toString());
+                    } catch (NumberFormatException e) {
+                        mKmPerHourEditText.setText(R.string.errorMsg);
+                        Log.d("Test", "e:" + e);
+                    }
+                    return true;
+                }
+                return false;
             }
         });
     }
-
-
 }
+
+
+
+
+
 
 
