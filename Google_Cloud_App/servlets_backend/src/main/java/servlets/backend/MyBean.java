5bf3340fd1c8082a698c07d4c092c05e49ac5bf8
Marhary <marharyta_bakun@mail.ru>
2016-10-13 23:41:31 +0300

    Full App

---
+++ Google_Cloud_App/servlets_backend/src/main/java/servlets/backend/MyBean.java
@@ -0,0 +1,17 @@
+package servlets.backend;
+
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+
+        return myData;
+    }
+
+    public void setData(String data)
+    {
+        myData = data;
+    }
+}
+
