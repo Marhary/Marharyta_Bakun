21c816ee9862d0234701192caabc7faf55e50c25
Marhary <marharyta_bakun@mail.ru>
2016-10-30 16:36:31 +0300

    v 4.6

--- SaveURLife/app/src/main/java/com/example/margo/saveurlife/MainActivityListFragment.java
+++ SaveURLife/app/src/main/java/com/example/margo/saveurlife/MainActivityListFragment.java
@@ -7,14 +7,28 @@ import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainActivityListFragment extends ListFragment {
 
+    private ArrayList<Note> notes;
+    private NoteAdapter noteAdapter;
+
+
     @Override
     public void onActivityCreated(Bundle saveInstanceState) {
         super.onActivityCreated(saveInstanceState);
+
+        notes = new ArrayList<Note>();
+        notes.add(new Note("This is a new note title", "This is the body of note",
+                Note.Category.PERSONAL));
+
+        noteAdapter = new NoteAdapter(getActivity(), notes);
+
+        setListAdapter(noteAdapter);
     }
 
     @Override
---
+++ SaveURLife/app/src/main/java/com/example/margo/saveurlife/Note.java
@@ -0,0 +1,73 @@
+package com.example.margo.saveurlife;
+
+
+public class Note {
+    private String title, message;
+    private long noteId, dateCreatedMilli;
+    private Category category;
+
+    public enum Category {PERSONAL, IDEAS, INTERESTING, JOB}
+
+    public Note(String title, String message, Category category) {
+        this.title = title;
+        this.message = message;
+        this.category = category;
+        this.noteId = 0;
+        this.dateCreatedMilli = 0;
+    }
+
+    public Note(String title, String message, Category category, long noteId, long dateCreatedMilli) {
+        this.title = title;
+        this.message = message;
+        this.category = category;
+        this.noteId = noteId;
+        this.dateCreatedMilli = dateCreatedMilli;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public long getDate() {
+        return dateCreatedMilli;
+    }
+
+
+    public long getId() {
+        return noteId;
+    }
+
+    public String toString() {
+        return "ID: " + noteId + " Title: " + title + " Message: " + message + " IconID: " + category.name()
+                + " Data: ";
+    }
+
+    public int getAssociatedDrawable() {
+        return categoryToDrawable(category);
+    }
+
+    public static int categoryToDrawable(Category noteCategory){
+
+        switch (noteCategory){
+            case PERSONAL:
+                return R.drawable.p;
+            case IDEAS:
+                return R.drawable.t;
+            case JOB:
+                return R.drawable.f;
+            case INTERESTING:
+                return R.drawable.q;
+        }
+        return R.drawable.p;
+    }
+}
+
+
