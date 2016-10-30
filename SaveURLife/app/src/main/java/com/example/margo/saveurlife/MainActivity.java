4ca1764e7387290c447e22b9aaed802e87f90ab7
Marhary <marharyta_bakun@mail.ru>
2016-10-30 16:56:25 +0300

    v 5.0

--- SaveURLife/app/src/main/java/com/example/margo/saveurlife/MainActivity.java
+++ SaveURLife/app/src/main/java/com/example/margo/saveurlife/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String NOTE_ID_EXTRA = "com.example.margo.saveurlife.Note Identifier";
+    public static final String NOTE_TITLE_EXTRA = "com.example.margo.saveurlife.Note Title";
+    public static final String NOTE_MESSAGE_EXTRA = "com.example.margo.saveurlife.Note Message";
+    public static final String NOTE_CATEGORY_EXTRA = "com.example.margo.saveurlife.Note Category";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- SaveURLife/app/src/main/java/com/example/margo/saveurlife/MainActivityListFragment.java
+++ SaveURLife/app/src/main/java/com/example/margo/saveurlife/MainActivityListFragment.java
@@ -2,6 +2,7 @@ package com.example.margo.saveurlife;
 
 
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -44,6 +45,24 @@ public class MainActivityListFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
+        
+        launchNoteDetailActivity(position);
     }
 
+    private void launchNoteDetailActivity(int position){
+
+        //note information
+        Note note = (Note) getListAdapter().getItem(position);
+
+        //create a new intent that launches noteDetailActivity
+        Intent intent = new Intent(getActivity(), NoteDetailActivity.class);
+
+        //pass information of the note which click on nDA
+        intent.putExtra(MainActivity.NOTE_TITLE_EXTRA, note.getTitle());
+        intent.putExtra(MainActivity.NOTE_MESSAGE_EXTRA, note.getMessage());
+        intent.putExtra(MainActivity.NOTE_CATEGORY_EXTRA, note.getCategory());
+        intent.putExtra(MainActivity.NOTE_ID_EXTRA, note.getId());
+
+        startActivity(intent);
+    }
 }
