c638f1b46567a2d87058b1d42284a2d8ba284a2e
Marhary <marharyta_bakun@mail.ru>
2016-11-01 15:13:00 +0300

    v 6.6

--- SaveURLife/app/src/main/java/com/example/margo/saveurlife/NoteEditFragment.java
+++ SaveURLife/app/src/main/java/com/example/margo/saveurlife/NoteEditFragment.java
@@ -6,9 +6,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -19,8 +21,9 @@ import android.widget.ImageButton;
 public class NoteEditFragment extends Fragment {
 
     private ImageButton noteCatButton;
+    private EditText title, message;
     private Note.Category savedButtonCategory;
-    private AlertDialog categoryDialogObject;
+    private AlertDialog categoryDialogObject, confirmDialogObject;
 
     public NoteEditFragment() {
         // Required empty public constructor
@@ -33,23 +36,33 @@ public class NoteEditFragment extends Fragment {
 
         View fragmentLayout = inflater.inflate(R.layout.fragment_note_edit, container, false);
 
-        EditText title = (EditText) fragmentLayout.findViewById(R.id.editNoteTitle);
-        EditText message = (EditText) fragmentLayout.findViewById(R.id.editNoteMessage);
+        title = (EditText) fragmentLayout.findViewById(R.id.editNoteTitle);
+        message = (EditText) fragmentLayout.findViewById(R.id.editNoteMessage);
         noteCatButton = (ImageButton) fragmentLayout.findViewById(R.id.editNoteButton);
+        Button savedButton = (Button) fragmentLayout.findViewById(R.id.saveNote);
 
         Intent intent = getActivity().getIntent();
         title.setText(intent.getExtras().getString(MainActivity.NOTE_TITLE_EXTRA));
         message.setText(intent.getExtras().getString(MainActivity.NOTE_MESSAGE_EXTRA));
 
         Note.Category noteCat = (Note.Category) intent.getSerializableExtra(MainActivity.NOTE_CATEGORY_EXTRA);
+        savedButtonCategory = noteCat;
         noteCatButton.setImageResource(Note.categoryToDrawable(noteCat));
 
         buildCategoryDialog();
+        buildConfirmDialog();
 
         noteCatButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 categoryDialogObject.show();
+            }
+        });
+
+        savedButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                confirmDialogObject.show();
             }
         });
 
@@ -89,6 +102,32 @@ public class NoteEditFragment extends Fragment {
         });
 
         categoryDialogObject = categoryBuilder.create();
+    }
+
+    private void buildConfirmDialog(){
+        AlertDialog.Builder confirmBuilder = new AlertDialog.Builder(getActivity());
+        confirmBuilder.setTitle("Are you sure?");
+        confirmBuilder.setMessage("Are you sure you want to save the note?");
+
+        confirmBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("Save Note", "Note title: " + title.getText() + "Note message: " +
+                        message.getText() + "Note category: " + savedButtonCategory);
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        confirmBuilder.setNegativeButton("Cansel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //do nothing
+            }
+        });
+
+        confirmDialogObject = confirmBuilder.create();
     }
 
 }
